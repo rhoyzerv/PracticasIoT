3844fa8e68e41b44a82a411df73bb7ec112221d3
Rogerte <rocuevasxe@gmail.com>
2023-02-16 22:03:01 -0600

    Still WIP

--- PRACTICA_4/servidor.c
+++ PRACTICA_4/servidor.c
@@ -5,10 +5,56 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <pthread.h>
-#include <cJSON.h>
+#include <cjson/cJSON.h>
+
 
 #define PORT 8080
-#define MAX_CLIENTS 1
+#define MAX_CLIENTS 5
+
+int json(){
+    // Open the JSON file
+  FILE* file = fopen("data.json", "r");
+  if (!file) {
+      printf("Failed to open file\n");
+      return 1;
+  }
+
+  // Read the contents of the file into a string
+  fseek(file, 0, SEEK_END);
+  long length = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char* data = (char*)malloc(length + 1);
+  fread(data, 1, length, file);
+  data[length] = '\0';
+
+  // Parse the JSON string
+  cJSON* root = cJSON_Parse(data);
+  if (!root) {
+      printf("Failed to parse JSON\n");
+      return 1;
+  }
+
+  // Get the array from the root object
+  cJSON* array = cJSON_GetObjectItem(root, "");
+
+  // Iterate over the array and print the values of each object
+  for (int i = 0; i < cJSON_GetArraySize(array); i++) {
+      cJSON* object = cJSON_GetArrayItem(array, i);
+      const char* name = cJSON_GetObjectItem(object, "name")->valuestring;
+      int x = cJSON_GetObjectItem(object, "x")->valueint;
+      int y = cJSON_GetObjectItem(object, "y")->valueint;
+      int z = cJSON_GetObjectItem(object, "z")->valueint;
+      int xyz = cJSON_GetObjectItem(object, "xyz")->valueint;
+      printf("Name: %s, x: %d, y: %d, z: %d, xyz: %d\n", name, x, y, z, xyz);
+  }
+
+  // Free memory and close the file
+  cJSON_Delete(root);
+  free(data);
+  fclose(file);
+
+  return 0;
+}
 
 void *handle_client(void *arg) {
   int client_socket = *(int *)arg;
@@ -21,6 +67,7 @@ void *handle_client(void *arg) {
   // Send data to the client
   //char *hello = "Hello from server";
   send(client_socket , buffer , strlen(buffer) , 0);
+  json();
   printf("Message sent to client\n");
 
   // Close the socket
@@ -28,6 +75,7 @@ void *handle_client(void *arg) {
 
   return NULL;
 }
+
 
 int main(int argc, char const *argv[]) {
   int server_socket, new_socket, valread;
@@ -59,7 +107,6 @@ int main(int argc, char const *argv[]) {
     perror("listen");
     exit(EXIT_FAILURE);
   }
-
   printf("Server is listening on port %d\n", PORT);
 
   // Create a thread for each incoming connection
